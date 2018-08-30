c204585dc0e39c42bbadef8f0aa78ed5c831621f
Gabriel Aumala <gabrielfima@live.com>
2018-08-30 16:26:55 -0500

    link fontfaces only when JS is disabled
    move all css for post pages into "css/posts" and merge them as
    "css/post-bundle.css" using the new template "concat.txt"
    Delete a lot of files generated by hakyll that where no longer used.
    Move al font face declarations to "css/font-faces.css". All pages
    reference this under a noscript tag, so that they are only loaded when
    JS is disabled.

--- static/site.hs
+++ static/site.hs
@@ -11,15 +11,9 @@ main = hakyll $ do
         route   idRoute
         compile copyFileCompiler
 
-    match "css/*" $ do
+    match ( "css/*" .||. "css/**/*") $ do
         route   idRoute
         compile compressCssCompiler
-
-    match (fromList ["about.rst", "contact.markdown"]) $ do
-        route   $ setExtension "html"
-        compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/default.html" defaultContext
-            >>= relativizeUrls
 
     match "posts/*" $ do
         route $ setExtension "html"
@@ -28,20 +22,14 @@ main = hakyll $ do
             >>= loadAndApplyTemplate "templates/post.html"    postCtx
             >>= relativizeUrls
 
-    create ["archive.html"] $ do
-        route idRoute
-        compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
-            let archiveCtx =
-                    listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "Archives"            `mappend`
-                    defaultContext
+    create ["css/post-bundle.css"] $ do
+      route idRoute
+      compile $ do
+        cssFiles <- loadAll "css/posts/*" 
+        let styleCtx = listField "items" defaultContext (return cssFiles)
 
-            makeItem ""
-                >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
-                >>= loadAndApplyTemplate "templates/default.html" archiveCtx
-                >>= relativizeUrls
-
+        makeItem ""
+            >>= loadAndApplyTemplate "templates/concat.txt" styleCtx
 
     match "index.html" $ do
         route idRoute
