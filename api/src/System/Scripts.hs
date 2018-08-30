672db4fcadf0d14269c2db7805ec896be0e1221a
Gabriel Aumala <gabrielfima@live.com>
2018-08-30 10:29:39 -0500

    Pipe bash script output to /dev/null

--- api/src/System/Scripts.hs
+++ api/src/System/Scripts.hs
@@ -1,17 +1,19 @@
 module System.Scripts (runUpdateScriptAtDir) where
 
 import System.Process
+import System.IO (IOMode(WriteMode))
 import System.Exit (ExitCode)
+import GHC.IO.Handle.FD (openFile)
 
 updateScript = proc "bash" ["-eo", "pipefail", "update.sh"]
 
 runUpdateScriptAtDir :: FilePath -> IO ExitCode
 runUpdateScriptAtDir workingDir = do
+  devNullHandle <- openFile "/dev/null" WriteMode
   (_, _, _, process) <- createProcess updateScript {
     cwd = Just workingDir, 
-    std_in = NoStream, 
-    std_out = CreatePipe, 
-    std_err = CreatePipe
+    std_out = UseHandle devNullHandle, 
+    std_err = UseHandle devNullHandle
   }
   waitForProcess process
 
