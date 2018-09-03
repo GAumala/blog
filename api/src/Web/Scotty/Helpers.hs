739ff318f2508b1c5aa6dc2f7ed8d77baee0fcb0
Gabriel Aumala <gabrielfima@live.com>
2018-09-02 23:55:35 -0500

    Truncate user input before storing to DB

--- api/src/Web/Scotty/Helpers.hs
+++ api/src/Web/Scotty/Helpers.hs
@@ -5,9 +5,10 @@ module Web.Scotty.Helpers (
   APIError (httpStatus, msg), 
   getIpAddress, 
   getUserAgent,
+  truncateUserInput,
   whenValid) where
 
-import Data.Text.Lazy (Text)
+import qualified Data.Text.Lazy as T
 import Network.HTTP.Types.Status (Status, badRequest400, status500)
 import Web.Scotty (ActionM, header, status, text) 
 
@@ -15,14 +16,20 @@ import Data.Models (
   IPAddress (IPAddress),
   UserAgent (UserAgent))
 
-data APIError = APIError { httpStatus :: Status, msg :: Text }
+data APIError = APIError { httpStatus :: Status, msg :: T.Text }
 
+truncateUserInput :: T.Text -> T.Text
+truncateUserInput input = truncated
+  where maxLength =  140
+        truncated = if T.length input < maxLength 
+                        then input 
+                        else T.take maxLength input `mappend` "..." 
 
 getUserAgent :: ActionM (Either APIError UserAgent)
 getUserAgent = do
   maybeUserAgent <- header "user-agent"  
   case maybeUserAgent of
-    Just userAgent -> return $ Right $ UserAgent userAgent
+    Just userAgent -> return $ Right $ UserAgent $ truncateUserInput userAgent
     Nothing -> return $ Left $ APIError { 
                     httpStatus = badRequest400, 
                     msg = "Missing user agent header" }
