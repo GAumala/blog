a28eeeb606e3226103717f001ff994119b783128
Gabriel Aumala <gaumala@protonmail.com>
2020-01-27 09:54:06 -0500

    Support manual deployment

--- .circleci/push_to_server.sh
+++ .circleci/push_to_server.sh
@@ -1,4 +1,4 @@
-if [ "${CIRCLE_BRANCH}" != "master" ]; then
+if [[ -n "$CIRCLE_BRANCH" && "$CIRCLE_BRANCH" != "master" ]]; then
   echo "Only master branch can deploy. Bye!"
   exit 0
 fi
@@ -7,14 +7,20 @@ mv ./static/_site /tmp
 git checkout server
 rm -rf _site
 mv /tmp/_site . 
+# bring source files that should also be deployed
 git checkout master -- .circleci/config.yml .gitignore nginx api 
 
 # Only push to server if something did change
 if [[ `git status --porcelain` ]]; then
-  git config --global user.email job@circleci.com
-  git config --global user.name CircleCI
   git add .
-  git commit -m "deploy #$CIRCLE_BUILD_NUM"
+
+  if [ -n "$CIRCLE_BUILD_NUM" ]; then
+    git config --global user.email job@circleci.com
+    git config --global user.name CircleCI
+    git commit -m "deploy #$CIRCLE_BUILD_NUM"
+  else
+    git commit -m "deploy manually"
+
   git push origin server
 else
   echo "No changes to deploy. Bye!"
